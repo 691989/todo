01c359af03aacd99edb5bd472840dce65621a93a
691989 <dhananjaylavate1995@gmail.com>
2020-01-22 13:03:06 -0500

    fourth commit

--- restful-web-services/src/main/java/com/todo/java/backend/rest/webservices/restfulwebservices/todo/TodoHardCodedService.java
+++ restful-web-services/src/main/java/com/todo/java/backend/rest/webservices/restfulwebservices/todo/TodoHardCodedService.java
@@ -29,6 +29,20 @@ public class TodoHardCodedService {
 		
 	}
 	
+	
+	public Todo saveUpdate(Todo todo) {
+		if(todo.getId()==-1 ||todo.getId()==0) {
+			todo.setId(++idCounter);
+			todos.add(todo);
+		}else {
+			deleteById(todo.getId());
+			todos.add(todo);		
+		}
+		return todo;
+	}
+	
+	
+	
 	public Todo deleteById(long id) {
 		Todo todo = findById(id);
 		if(todo==null) {
@@ -55,5 +69,5 @@ public class TodoHardCodedService {
 		
 		return null;
 	}
-	
+	 
 }
